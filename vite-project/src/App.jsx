0d9d30f20cd26368134345f3e22278f0ea48fe38
Kemal362 <pskkemalakalin@gmail.com>
2026-02-27 16:10:42 +0300

    alsjklşdkaşlskd

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,4 @@
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
